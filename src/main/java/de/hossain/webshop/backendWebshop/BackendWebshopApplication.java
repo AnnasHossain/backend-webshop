e34af639ebb8bcfbefcf8d1890e2d3c3eb55cf3c
AnnasHossain <annas.hossain@yahoo.de>
2024-12-21 18:17:54 +0100

    initial commit

---
+++ src/main/java/de/hossain/webshop/backendWebshop/BackendWebshopApplication.java
@@ -0,0 +1,13 @@
+package de.hossain.webshop.backendWebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hossain/webshop/backendWebshop/BackendWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hossain.webshop.backendWebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
